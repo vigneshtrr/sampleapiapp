cacf49a4b1e0f24322e94e843bc97732ac45d518
vignesh Kumar <vignesh.kumar@thoughthub.net>
2021-11-13 11:23:10 +0530

    wether forecast update

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace samplecorewebapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","sample"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
